3e3ba139deec5ae0b136091a5933aa27c2634fe1
Nata <saginadzenata@gmail.com>
2026-03-23 15:37:41 +0400

    Firebase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+import { environment } from '../environment/environment';
+
+
+export const firebaseApp = initializeApp(environment.firebase);
+export const db = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp);
+export const auth = getAuth(firebaseApp);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+  ],
 };
---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC70nwp3-wuy5CstvQ9FW1EUY392Y4AAMM",
+    authDomain: "clothing-lab.firebaseapp.com",
+    projectId: "clothing-lab",
+    storageBucket: "clothing-lab.firebasestorage.app",
+    messagingSenderId: "595774666718",
+    appId: "1:595774666718:web:11e6f7887b8b2ca0ec4f4f",
+    measurementId: "G-JBNVLL5J5X"
+  }
+};
